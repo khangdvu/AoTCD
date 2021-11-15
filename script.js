881f9dfdf4d6d25d8e094d617dc1f2f467b87e52
Khang Vu <khang.dinh.vu@gmail.com>
2021-11-15 15:52:06 -0800

    First commit

---
+++ script.js
@@ -0,0 +1,27 @@
+const digits_day = document.getElementById('digits--day')
+const digits_hour = document.getElementById('digits--hour')
+const digits_minute = document.getElementById('digits--minute')
+const digits_second = document.getElementById('digits--second')
+const text_info = document.getElementById('text--info')
+const releaseDate = new Date('January 9, 2022 00:00:00')
+
+setInterval(setCountdown, 1000);
+
+function setCountdown(){
+    let currentDate = new Date();
+    let cdTime = releaseDate.getTime() - currentDate.getTime();
+    if(cdTime > 0){
+        let cdDay = Math.floor(cdTime/1000/60/60/24);
+        let cdHour = Math.floor(cdTime/1000/60/60 % 24) - 1 ;
+        let cdMinute = Math.floor((cdTime/1000/60) % 60);
+        let cdSecond = Math.floor((cdTime/1000) % 60);
+    
+        digits_day.innerHTML = cdDay;
+        digits_hour.innerHTML = cdHour;
+        digits_minute.innerHTML = cdMinute;
+        digits_second.innerHTML = cdSecond;
+    }
+    else{
+        text_info.innerHTML= "AoT IS RELEASED! GO WATCH!"
+    }
+}
